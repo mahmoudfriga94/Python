1e0dc0feefbd3b96d8c57fff29f4f6183f5b5b2a
mahmoudfriga94 <mahmoudfriga94@gmail.com>
2021-08-21 07:05:51 +0200

    Add Circle.py

---
+++ Circle.py
@@ -0,0 +1,6 @@
+import math
+redius = float(input("Enter the redius: "))
+circumference = 2*math.pi*redius
+area = math.pi*(redius**2)
+print("circumference = "+ str(circumference))
+print("area = "+ str(area))
